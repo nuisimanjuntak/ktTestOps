7a7162494955677c0030ba7f43bbdd24560a6580
nuijo <nuijo@NuiJS>
2025-03-13 15:44:02 +0700

    first commit

---
+++ Test Suites/login.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>login</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>56585101-1b88-4577-ac83-c535437bdfb3</testSuiteGuid>
+</TestSuiteEntity>
